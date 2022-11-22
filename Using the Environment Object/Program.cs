5ef1a24e076307ffc064129130d65f7b36e97397
Okafornta <hillaryonia@gmail.com>
2022-11-22 10:05:32 +0100

    Add project files.

---
+++ Using the Environment Object/Program.cs
@@ -0,0 +1,7 @@
+﻿// Displaying components of Enviroment object
+Console.WriteLine("Device name: " + Environment.MachineName);
+Console.WriteLine("64-bit system: " + Environment.Is64BitOperatingSystem);
+Console.WriteLine("Username: " + Environment.UserName);
+
+// Waiting for Enter
+Console.ReadLine();
